db60b8ab7c849b96063fe9488cf532cd5ea707c4
Sakib-203-15-3883 <sakibul15.3883@gmail.com>
2024-09-07 11:20:04 +0600

    implement useState related feature

--- screens/reactHooks/UseState.jsx
+++ screens/reactHooks/UseState.jsx
@@ -1,19 +1,67 @@
-import { ScrollView, StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import React, {useState} from 'react';
 
-const UseState = () => {
+const LearnUseState = () => {
+  const [backgroundColor, setBackgroundColor] = useState('black');
+
+  const changeBackgroundToGreen = () => {
+    setBackgroundColor('green');
+  };
+
+  const changeBackgroundToBlack = () => {
+    setBackgroundColor('black');
+  };
+
   return (
-    <ScrollView style={styles.container}>
-      <Text>UseState</Text>
+    <ScrollView
+      contentContainerStyle={styles.scrollView}
+      style={{backgroundColor: backgroundColor}}>
+      {backgroundColor === 'black' ? (
+        <View style={styles.container}>
+          <TouchableOpacity
+            onPress={changeBackgroundToGreen}
+            style={styles.button}>
+            <Text style={styles.text}>Set Background color to green </Text>
+          </TouchableOpacity>
+        </View>
+      ) : (
+        <View style={styles.container}>
+          <TouchableOpacity
+            onPress={changeBackgroundToBlack}
+            style={styles.button}>
+            <Text style={styles.text}>Set Background color to black </Text>
+          </TouchableOpacity>
+        </View>
+      )}
     </ScrollView>
-  )
-}
+  );
+};
 
-export default UseState
+export default LearnUseState;
 
 const styles = StyleSheet.create({
-  container:{
-    flex:1,
-    backgroundColor:"black"
-  }
-})
+  scrollView: {
+    flexGrow: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  container: {
+    alignItems: 'center',
+  },
+  button: {
+    borderWidth: 2,
+    borderColor: 'red',
+    borderRadius: 20,
+    padding: '3%',
+  },
+  text: {
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+});
